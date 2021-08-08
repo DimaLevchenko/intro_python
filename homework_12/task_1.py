c5f67468717e8f125312b01a1c4813fac296e039
DimaLevchenko <85802012+DimaLevchenko@users.noreply.github.com>
2021-08-08 22:59:11 +0300

    Add files via upload

--- homework_12/task_1.py
+++ homework_12/task_1.py
@@ -10,6 +10,38 @@
 #   При изменении баланса записывать в транзакции (сумма, тип операции, текущая_дата)
 #   * доп. добавить и учитывать банковские комиссии (1%)
 
+from uuid import uuid4
+from datetime import datetime
 
 
+class BankAccount:
+    def __init__(self, name: str, balance: float):
+        self.name = name
+        self.id = uuid4()
+        self.balance = balance
+        self.transaction = []
 
+    def operations(self, operation: float):
+        commission = 0.01
+        sum_of_commission = operation * commission
+        sum_of_operation = operation - sum_of_commission
+        return sum_of_operation
+
+    def deposit(self, operator: float):
+        self.balance += self.operations(operator)
+        self.transaction.append([self.operations(operator), 'deposit', datetime.today().strftime('d%-%m-%Y')])
+        answer = f'Your deposit is {self.operations(operator)}, your balance is {self.balance}'
+        return answer
+
+    def withdrawal(self, operator: float):
+        self.balance -= self.operations(operator)
+        self.transaction.append([self.operations(operator), 'withdrawal', datetime.today().strftime('d%-%m-%Y')])
+        answer = f'Your withdrawal is {self.operations(operator)}, your balance is {self.balance}'
+        return answer
+
+    def get_balance(self):
+        answer = f'Your balance is {self.balance}'
+        return answer
+
+
+first_account = BankAccount('First', 1000)
